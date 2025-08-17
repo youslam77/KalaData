20ceaf813fd81436225019376851763531cb2197
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-08-17 23:51:04 +0300

    setting up compression and decompression

--- src/compression/compress.cpp
+++ src/compression/compress.cpp
@@ -4,6 +4,9 @@
 //Read LICENSE.md for more information.
 
 #include <filesystem>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 #include "compression/compress.hpp"
 #include "core/core.hpp"
@@ -12,6 +15,34 @@ using KalaData::Core::KalaDataCore;
 using KalaData::Core::MessageType;
 
 using std::filesystem::path;
+using std::filesystem::create_directories;
+using std::filesystem::relative;
+using std::filesystem::is_regular_file;
+using std::filesystem::recursive_directory_iterator;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+using std::streamoff;
+using std::istreambuf_iterator;
+using std::vector;
+using std::stringstream;
+using std::ostringstream;
+
+constexpr size_t WINDOW_SIZE = 4096; //4KB sliding window
+constexpr size_t LOOKAHEAD = 18;     //Max match length
+constexpr size_t MIN_MATCH = 3;
+
+struct Token
+{
+	bool isLiteral;
+	uint8_t literal;
+	uint16_t offset;
+	uint8_t length;
+};
+
+static void CompressBuffer(const vector<uint8_t>&, ofstream& out);
+
+static vector<uint8_t> DecompressBuffer(ifstream& in, size_t compressedSize);
 
 namespace KalaData::Compression
 {
@@ -19,11 +50,8 @@ namespace KalaData::Compression
 		const string& origin,
 		const string& target)
 	{
-		string archivePath = (path(target) / path(target).stem()).string() + ".kdat";
-		string archiveName = path(archivePath).filename().string();
-
 		KalaDataCore::PrintMessage(
-			"Starting to compress folder '" + origin + "' to archive '" + archiveName + "' in target folder '" + target + "'!\n",
+			"Starting to compress folder '" + origin + "' to archive '" + target + "'!\n",
 			MessageType::MESSAGETYPE_DEBUG);
 	}
 
@@ -31,11 +59,18 @@ namespace KalaData::Compression
 		const string& origin,
 		const string& target)
 	{
-		string archivePath = (path(origin) / path(origin).stem()).string() + ".kdat";
-		string archiveName = path(archivePath).filename().string();
-
 		KalaDataCore::PrintMessage(
-			"Starting to decompress archive '" + archivePath + "' to folder '" + target + "'!\n",
+			"Starting to decompress archive '" + origin + "' to folder '" + target + "'!\n",
 			MessageType::MESSAGETYPE_DEBUG);
 	}
+}
+
+void CompressBuffer(const vector<uint8_t>&, ofstream& out)
+{
+
+}
+
+vector<uint8_t> DecompressBuffer(ifstream& in, size_t compressedSize)
+{
+
 }
